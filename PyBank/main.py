b90336d4434d44632270a9c6b079cbdcdaebfb23
rca 2123 <43800091+rachaelcamick@users.noreply.github.com>
2018-11-01 23:36:01 -0400

    i have no comment

---
+++ PyBank/main.py
@@ -0,0 +1,11 @@
+# Dependencies
+import os
+import csv
+# path to collect data from budget file
+csvpath = os.path.join("budget_data")
+# make empty lists
+months = []
+net_profit =[]
+monthly_profit = []
+
+
---
+++ PyBank/test.py
@@ -0,0 +1,5 @@
+import csv
+with open("budget_data.csv", "r") as file:
+    filereader = csv.reader(file)
+    for row in filereader:
+        print row
