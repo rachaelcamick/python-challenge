b0e72387ae8131a67dc3f3dd0986bae4215cf358
rca 2123 <43800091+rachaelcamick@users.noreply.github.com>
2018-11-01 23:54:59 -0400

    still working

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,5 +7,13 @@ csvpath = os.path.join("budget_data")
 months = []
 net_profit =[]
 monthly_profit = []
+#open file
+with open(csvpath, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    #skip header so list includes only values
+    skipheader = next(csvreader)
+    
+
+
 
 
--- PyBank/test.py
+++ PyBank/test.py
@@ -2,4 +2,4 @@ import csv
 with open("budget_data.csv", "r") as file:
     filereader = csv.reader(file)
     for row in filereader:
-        print row
+        print(row[0])
