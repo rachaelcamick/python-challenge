1a6f426cda8ece94b07f5c86ec57386ef6bbd868
rca 2123 <43800091+rachaelcamick@users.noreply.github.com>
2018-11-02 20:10:22 -0400

    looping through csv to find monthly profit change

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,19 +6,17 @@ csvpath = os.path.join("budget_data")
 # make empty lists
 months = []
 net_profit =[]
-monthly_profit = []
+monthly_profit_delta = []
 #open file
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     #skip header so list includes only values
     skipheader = next(csvreader)
-    # now loop through rows
+    # loop through rows in the data file
     for row in csvreader: 
         months.append(row[0])
         net_profit.append(row[1])
-        
-
-
-
-
-
+    # loop through the length of the list
+    for i in range(len(net_profit)):
+        monthly_profit_delta.append(net_profit(i+1)-net_profit(i))
+    
