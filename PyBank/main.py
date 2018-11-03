7b1fee3138493bd0ed83267b9bae0e9949beafa9
rca 2123 <43800091+rachaelcamick@users.noreply.github.com>
2018-11-02 21:35:46 -0400

    month

--- PyBank/main.py
+++ PyBank/main.py
@@ -17,8 +17,14 @@ with open(csvpath, newline='') as csvfile:
         months.append(row[0])
         net_profit.append(row[1])
     # loop through the length of the list
-    for i in range(len(net_profit)):
+    for i in list(len(net_profit)):
+        #make a list comprised of the profit changes between months
         monthly_change.append(net_profit(i+1)-net_profit(i))
+# find monthly max and min
 increase_max = max(monthly_change)
 decrease_max = min(monthly_change)
+# identify the month based on the index
+increase_month = monthly_change.index(max(monthly_change))
+decrease_month = monthly_change.index(min(monthly_change))
+
 
