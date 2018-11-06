4bcf6e411102c8f9f67c65e70bca461792616c58
rca 2123 <43800091+rachaelcamick@users.noreply.github.com>
2018-11-06 07:32:52 -0500

    added skipheader

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,9 +6,13 @@ csvpath = os.path.join("election_data.csv")
 #read path using csv module
 with open(csvpath, newline='') as csvfile
     read_election = csv.reader(csvfile, delimiter=',')
+    skipheader = next(read_election)
 
 #create list for total number of votes cast, candidates
 total_votes = []
 candidate_list = []
-#create list 
+#create list for number of votes per candidate
+votes_per_cand = []
+
+
 
