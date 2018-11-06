2fca858867e2b50322f85d677497713337b40016
rca 2123 <43800091+rachaelcamick@users.noreply.github.com>
2018-11-05 23:13:45 -0500

    resolved path issues, syntax errors in for loop

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,12 +2,11 @@
 import os
 import csv
 # path to collect data from budget file
-csvpath = os.path.join("budget_data")
+csvpath = os.path.join("budget_data.csv")
 # make empty lists
 months = []
 net_profit =[]
 monthly_change = []
-#open file
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     #skip header so list includes only values
@@ -15,24 +14,21 @@ with open(csvpath, newline='') as csvfile:
     # loop through rows in the data file
     for row in csvreader: 
         months.append(row[0])
-        net_profit.append(int(row[1])
+        net_profit.append(int(row[1]))
     # loop through the length of the list
-    for i in range(1, len(net_profit))
+    for i in range(1, len(net_profit)-1):
         #make a list comprised of the profit changes between months
-        monthly_change.append((int(net_profit(i+1)))-(int(net_profit(1))))
-# find monthly max and min
+        monthly_change.append((int(net_profit[i+1]) - int(net_profit[i])))
+#find monthly max and min
 increase_max = max(monthly_change)
 decrease_max = min(monthly_change)
-# identify the month based on the index
+#find greatest increase and decrease in profits 
 increase_month = monthly_change.index(max(monthly_change))
 decrease_month = monthly_change.index(min(monthly_change))
-#print statements
+# print statements
 print("Financial Analysis")
 print(f"Total Months: {len(months)}")
 print(f"Total: ${sum(net_profit)}")
-print(f"Average Change: {sum(monthly_change)/len(monthly_change)}")
-print(f"Greatest Increase in Profits: {months(increase_month)} ${str(increase_max)}")
-print(f"Greatest Decrease in Profits: {months(decrease_max)} ${str(decrease_max)}")
-
-
-
+print(f"Average Change: ${sum(monthly_change)/len(monthly_change)}")
+print(f"Greatest Increase in Profits: {months[increase_month]} ${str[increase_max]}")
+print(f"Greatest Decrease in Profits: {months[decrease_max]} ${str[decrease_max]}")
