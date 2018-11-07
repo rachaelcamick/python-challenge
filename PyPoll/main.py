fe1f7b7101d0cf1284708d35178755245809825b
rca 2123 <43800091+rachaelcamick@users.noreply.github.com>
2018-11-06 19:05:57 -0500

    start lists

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,7 +4,7 @@ import csv
 #path
 csvpath = os.path.join("election_data.csv")
 #read path using csv module
-with open(csvpath, newline='') as csvfile
+with open(csvpath, newline='') as csvfile:
     read_election = csv.reader(csvfile, delimiter=',')
     skipheader = next(read_election)
 
@@ -13,6 +13,10 @@ total_votes = []
 candidate_list = []
 #create list for number of votes per candidate
 votes_per_cand = []
+
+# loop through csvfile for total number of votes
+for row in read_election:
+    total_votes.append(row[1])
 
 
 
